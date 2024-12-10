b1a2d0f08549033893b659343267aa1986ff7dc6
dagousket <swann@thinkr.fr>
2024-12-10 15:08:07 +0000

    test: test quarto version and duplicated keyword

---
+++ tests/testthat/test-check_quarto_version.R
@@ -0,0 +1,13 @@
+test_that("check_quarto_version warns user with invalid quarto version", {
+  
+  expect_error(
+    object = {
+      with_mocked_bindings(
+        quarto_version = function(...){"1.0.0"},
+        code = {check_quarto_version()}
+      )
+    },
+    regexp = "minimal required version is 1.3.0"
+  )
+  
+})
---
+++ tests/testthat/test-fetch_future_settings.R
@@ -0,0 +1,26 @@
+test_that("future settings does not speak for default behaviour", {
+
+  # set to default
+  plan("default")
+  
+  expect_message(
+    object = {fetch_future_settings(quiet = FALSE)},
+    regexp = NA
+  )
+  
+})
+
+test_that("future settings does not speak for default behaviour", {
+  
+  # set a multisession with one worker
+  plan(future::multisession, workers = 1)
+  
+  expect_message(
+    object = {fetch_future_settings(quiet = FALSE)},
+    regexp = "\\{future\\} is using plan\\(future::multisession, workers = 1\\)"
+  )
+  
+  # reset to default
+  plan("default")
+  
+})
