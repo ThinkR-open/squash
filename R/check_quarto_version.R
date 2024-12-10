ca40180df24835f9fb87000d419f5b5a0ad33a1b
dagousket <swann@thinkr.fr>
2024-12-10 15:07:25 +0000

    refacto: factorise onLoad

---
+++ R/check_quarto_version.R
@@ -0,0 +1,22 @@
+#' Verify quarto version
+#' 
+#' @importFrom quarto quarto_version
+#'
+#' @return Null, warn user if quarto version is non compatible
+#' @noRd
+check_quarto_version <- function(){
+  # fetch quarto version
+  local_quarto <- quarto_version()
+
+  # raise a warning if quarto version is < 1.3.0
+  version_ok <-  local_quarto >= numeric_version("1.3.0")
+  
+  if (!version_ok){
+    stop(paste0(
+      "Your current version of quarto is ",
+      local_quarto,
+      ", minimal required version is 1.3.0"
+      )
+    )
+  }
+}
